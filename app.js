0e5326142d65e76273d4864922184b778ebf28cd
jili <jili@heidrick.com>
2017-07-20 18:33:50 -0500

    set port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || 5000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// this is key to use process.env.PORT!! to run this on Heroku
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
